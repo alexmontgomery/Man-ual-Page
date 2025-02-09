5f832f5facf6c63d7cf6f1be6fee9d1f1c4dfd70
alexmontgomery <alexrm.1212@gmail.com>
2025-02-08 16:40:31 -0800

    Fix - bind Flask to 0.0.0.0 for Render deployment

--- backend/server.py
+++ backend/server.py
@@ -43,4 +43,6 @@ class YearToSearch(Resource):
 api.add_resource(YearToSearch, '/api/search/<int:year>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
